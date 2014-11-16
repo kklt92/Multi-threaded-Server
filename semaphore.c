9682510406db44d44923848f2cb8c647528028ab
kklt92 <kklt92@gmail.com>
2014-11-16 17:38:39 -0500

    recover semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -20,25 +20,25 @@ int sem_wait(m_sem_t *s)
 {
   pthread_mutex_lock(&(s->lock));
 
-//  while(s->value <= 0) {
-//    pthread_cond_wait(&(s->notify), &(s->lock));
-//  }
+  while(s->value <= 0) {
+    pthread_cond_wait(&(s->notify), &(s->lock));
+  }
 
   
-//  s->value--;
+  s->value--;
   
-//  pthread_mutex_unlock(&(s->lock));
+  pthread_mutex_unlock(&(s->lock));
 
   return 0;
 }
 
 int sem_post(m_sem_t *s)
 {
-//  pthread_mutex_lock(&(s->lock));
+  pthread_mutex_lock(&(s->lock));
 
-//  s->value++;
+  s->value++;
 
- // pthread_cond_broadcast(&(s->notify));
+  pthread_cond_broadcast(&(s->notify));
 
   pthread_mutex_unlock(&(s->lock));
   return 0;
